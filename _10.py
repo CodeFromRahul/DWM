f513ec2ae8ee238d96079ca7ca870b45cb4cec6f
CodeFromRahul <rahulvish194002@gmail.com>
2024-11-04 02:37:48 +0530

    _10 complete

--- _10.py
+++ _10.py
@@ -9,10 +9,10 @@ def page_rank(links,beta=0.8,iteration=2):
 
         return (1-beta)+beta*rank_sum
 
-print(f"Iteration 0,{ranks}")
+    print(f"Iteration 0,{ranks}")
 
-for i in range(1,iteration+1):
-    new_ranks={}
+    for i in range(1,iteration+1):
+        new_ranks={}
     
     for page in links:
         new_ranks[page]= calculate_rank(page)
@@ -20,3 +20,23 @@ for i in range(1,iteration+1):
     ranks=new_ranks
 
     print(f"Iteration {i}:{ranks}")
+    
+    # final 
+    
+    print("\nFinal pagerank values:")
+    for page,rank in ranks.items():
+        print(f"page{page}:{rank}")
+
+    def get_user_input():
+        num_pages=int(input("Enter the number of pages :"))
+        links={}
+
+        for i in range(num_pages):
+            page=input("Enter page {i+1} name:")
+            outgoing=input(f"Enter the putgoin links from {page}(comma-separated):").split(',')
+            links[page]=[links.strip() for link in outgoing if links.strip()!='']
+            return links
+        
+    if _name_ =="_main_":
+        links =get_user_input()
+        page_rank(links)
